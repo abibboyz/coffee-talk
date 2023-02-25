adc5bf91709630146dfb92837cc2e2de2cfccc32
Abinash <abi.kh03@gmail.com>
2023-02-25 22:27:37 +1100

    login and session route added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  root 'main#index'
+  #root 'main#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
   # root "articles#index"
+  root 'chatroom#index'
+  get 'login', to: 'sessions#new'
 end
